88847c076d20d89a2b73de86d1b05290416f906e
Puja Rani <pr045739@gmail.com>
2025-08-24 11:14:57 +0530

    Update model

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -22,7 +22,7 @@ const generateInterviewQuestions = async (req, res) => {
     const prompt = questionAnswerPrompt(role, experience, topicsToFocus, numberOfQuestions);
     
     const response = await ai.models.generateContent({
-      model: "models/gemini-1.5-flash-latest",
+      model: "gemini-1.5-flash-latest",
       contents: prompt,
     });
 
